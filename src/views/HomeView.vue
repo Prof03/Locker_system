0e8838e500d077dd2c0ceff60771829eb287fc0f
Atlas-Dev-0 <kenneth.gonzales@bicol-u.edu.ph>
2024-09-12 20:04:33 +0800

    setted up vite environment

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <!-- sample of tailwind -->
+    <div class="flex-row container border">
+      <h1 class="border">
+        dito ka mag code kitts
+      </h1>
+    </div>
+  </main>
+</template>
